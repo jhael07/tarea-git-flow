d40598b1319c6cb785bc378e9687a5931b60e4bd
jhael07 <jhaelr007@gmail.com>
2023-11-25 19:30:55 -0400

    dev

---
+++ main.py
@@ -0,0 +1,18 @@
+def multiply(a:int,b:int):
+    return a * b
+
+print(multiply(2,5))
+
+def sum(a:int,b:int):
+    return a + b
+
+
+print(sum(1,4))
+print(sum(2,4))
+
+
+def subtract(a: int,b:int):
+    return a - b
+
+print(subtract(10,2))
+
